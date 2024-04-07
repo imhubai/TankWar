de53382b04e28f61573726bf9a0bdb2f2f92add3
狐白 <dream.qu@qq.com>
2024-04-07 16:18:44 +0800

    Version1.0 Create Missile class

---
+++ src/Missile.java
@@ -0,0 +1,47 @@
+import java.awt.*;
+
+public class Missile {
+    public static final int XSPEED = 10;
+    public static final int YSPEED = 10;
+    Tank.Direction dir;
+    private int x, y;
+
+    public Missile(int x, int y, Tank.Direction dir) {
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+    }
+
+    private void move() {
+        switch (dir) {
+            case L -> x -= XSPEED;
+            case LU -> {
+                x -= XSPEED;
+                y -= YSPEED;
+            }
+            case U -> y -= YSPEED;
+            case RU -> {
+                x += XSPEED;
+                y -= YSPEED;
+            }
+            case R -> x += XSPEED;
+            case RD -> {
+                x += XSPEED;
+                y += YSPEED;
+            }
+            case D -> y += YSPEED;
+            case LD -> {
+                x -= XSPEED;
+                y += YSPEED;
+            }
+        }
+    }
+
+    public void draw(Graphics g) {
+        Color c = g.getColor();
+        g.setColor(Color.BLACK);
+        g.fillOval(x, y, 10, 10);
+        g.setColor(c);
+        move();
+    }
+}
